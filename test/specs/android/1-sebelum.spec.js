c8cf798b7ac492d102642c5eca4d8a23916b7c48
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-12 11:46:52 +0700

    wip: add appium and run test on device

--- test/specs/android/1-sebelum.spec.js
+++ test/specs/android/1-sebelum.spec.js
@@ -1,3 +1,9 @@
 const LoginScreen = require("../../screenobjects/android/login.screen");
 const RekamanScreen = require("../../screenobjects/android/rekaman.screen");
 const InputGulaDarahScreen = require("../../screenobjects/android/input-gula-darah.screen");
+
+describe("test buka", () => {
+  it("test tunggu", async () => {
+    await driver.pause(3000);
+  });
+});
