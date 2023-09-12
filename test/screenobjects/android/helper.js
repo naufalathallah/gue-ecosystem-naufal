dba520e1fc9850790d3948e9a065cb06418787d1
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-12 13:49:35 +0700

    feat: reusable login logout

---
+++ test/screenobjects/android/helper.js
@@ -0,0 +1,22 @@
+const loginScreen = require("./login.screen");
+const logoutScreen = require("./logout.screen");
+
+async function login() {
+  await loginScreen.midDelayTime();
+  await loginScreen.btnSudahPunyaAkun.click();
+  await loginScreen.fillEmail.addValue(process.env.USER_EMAIL);
+  await loginScreen.fillPassword.addValue(process.env.USER_PASSWORD);
+  await loginScreen.btnSumbitLogin.click();
+  await loginScreen.shortDelayTime();
+  await loginScreen.closePopup();
+  await expect(loginScreen.btnBeranda).toBeDisplayed();
+}
+
+async function logout() {
+  await logoutScreen.btnProfile.click();
+  await logoutScreen.scrollToBot();
+  await logoutScreen.btnLogout.click();
+  await expect(loginScreen.fillEmail).toBeDisplayed();
+}
+
+module.exports = { login, logout };
--- test/specs/android/1-sebelum.spec.js
+++ test/specs/android/1-sebelum.spec.js
@@ -1,25 +1,11 @@
-const loginScreen = require("../../screenobjects/android/login.screen");
-const logoutScreen = require("../../screenobjects/android/logout.screen");
+const { login, logout } = require("./path/to/helper");
 
 describe("Sebagai user teman diabetes, saya dapat mencatat gula darah manual dengan pemilihan jenis pemeriksaan gula darah sebelum makan siang", () => {
   it("Login", async () => {
-    await loginScreen.midDelayTime();
-
-    await loginScreen.btnSudahPunyaAkun.click();
-    await loginScreen.fillEmail.addValue(process.env.USER_EMAIL);
-    await loginScreen.fillPassword.addValue(process.env.USER_PASSWORD);
-    await loginScreen.btnSumbitLogin.click();
-    await loginScreen.shortDelayTime();
-    await loginScreen.closePopup();
-
-    await expect(loginScreen.btnBeranda).toBeDisplayed();
+    await login();
   });
 
   it("Logout", async () => {
-    await logoutScreen.btnProfile.click();
-    await logoutScreen.scrollToBot();
-    await logoutScreen.btnLogout.click();
-
-    await expect(loginScreen.fillEmail).toBeDisplayed();
+    await logout();
   });
 });
